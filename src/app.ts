b9c91c0198a69ab7f40cd15bd7ce91cf956f9ba8
Showkot <showkotus@gmail.com>
2024-04-03 15:12:18 +0600

    test lint-stage

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 // const express = require('express')
-import express, { Application } from "express"
-import cors from "cors"
+import express, { Application, Request } from 'express'
+import cors from 'cors'
 const app: Application = express()
 
 app.use(cors())
 
 //parser
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Server runninng..')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 // const mongoose = require('mongoose');
-import mongoose from "mongoose";
-import app from "./app"
-import config from "./config/index"
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Database Connected Succesfully");
-        app.listen(config.port, () => {
-            console.log(`app listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log("Databse connection failed..")
-    }
-
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connected Succesfully')
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Databse connection failed..')
+  }
 }
 
 bootstrap()
